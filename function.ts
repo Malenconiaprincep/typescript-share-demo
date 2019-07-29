441e594c97302d0da8f040c41a44e138df8a16e8
makuta <wangbo12@wps.cn>
2019-07-29 18:01:56 +0800

    update

---
+++ function.ts
@@ -0,0 +1,10 @@
+// Named function
+function add(x: number, y: number): number {
+  return x + y;
+}
+
+
+// Anonymous function
+function add2(x: number, y: number): number {
+  return x + y;
+}
---
+++ generics.ts
@@ -0,0 +1,9 @@
+// 泛型
+
+function identity(arg: any): any {
+  return arg;
+}
+
+// function identity<T>(arg: T): T {
+//   return arg;
+// }
